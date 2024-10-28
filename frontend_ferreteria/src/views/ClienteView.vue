3b5647ff553858caca309bd98ad89a3e690b5075
jhon1207 <jhonnyzunigam12@gmail.com>
2024-10-27 23:47:35 -0400

    Frontend Ferreteria Entidad Clientes

---
+++ frontend_ferreteria/src/views/ClienteView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import ClienteList from '@/components/cliente/ClienteList.vue'
+import ClienteSave from '@/components/cliente/ClienteSave.vue'
+import Button from 'primevue/button'
+import { ref } from 'vue'
+
+const mostrarDialog = ref<boolean>(false)
+const clienteListRef = ref<typeof ClienteList | null>(null)
+const clienteEdit = ref<any>(null)
+
+function hableCreate() {
+  clienteEdit.value = null
+  mostrarDialog.value = true
+}
+
+function handleEdit(cliente: any) {
+  clienteEdit.value = cliente
+  mostrarDialog.value = true
+}
+
+function handleCloseDialog() {
+  mostrarDialog.value = false
+}
+
+function handleGuardar() {
+  clienteListRef.value?.obtenerLista()
+}
+</script>
+
+<template>
+  <div>
+    <h1>Intépretes</h1>
+    <Button label="Crear Nuevo" icon="pi pi-plus" @click="hableCreate" />
+    <ClienteList ref="clienteListRef" @edit="handleEdit" />
+    <ClienteSave :mostrar="mostrarDialog" :cliente="clienteEdit" :modoEdicion="!!clienteEdit" @guardar="handleGuardar"
+      @close="handleCloseDialog" />
+  </div>
+</template>
+
+<style scoped></style>
